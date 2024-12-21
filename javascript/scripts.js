3e7c96a548986ebffcf4d94a08abab81ff709458
zynologics <morrissprokholt12@gmail.com>
2024-12-21 17:08:36 +0100

    Side menu werkt

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -43,3 +43,27 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.toggle('opened');
     });
 });
+
+document.getElementById('skills-button').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Scroll to the skills section
+    const skillsSection = document.getElementById('skills-selection');
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+
+    skillsSection.classList.add('animate');
+
+    // setTimeout(function() {
+    //     const skillBars = document.querySelectorAll('.skill-per');
+    //     skillBars.forEach(bar => {
+    //         bar.style.width = '0'; // Reset width to 0
+    //         bar.offsetHeight; // Trigger reflow
+
+    //         // Set the final width first
+    //         bar.style.width = bar.getAttribute('data-per'); // Set width to the data-per value
+
+    //         // Add the animation class
+    //         bar.classList.add('fill-animation');
+    //     });
+    // }, 500); // Adjust this delay as needed
+});
